1eb7a253fbb6e94a67f7d544364e8918cec04643
kaushalrai7797 <kaushal7797@gmail.com>
2022-05-07 00:09:26 -0500

    Code for Sentence Transformer and requirements.txt

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-# SEAL-Sentence-Embedding-approach-towards-Active-Learning
+# SCALe: Supervised Contrastive approach for Active Learning
 
-# Quick Overview
+## To reproduce our results please run the following commands
 
-Steps to run the code -
-1. seal.py has the logic to generate the results that we show in our report.
-2. The file currently accepts no parameters. However you can switch between three datasets within the file - SST-2, IMDB and TREC-6.
-3. The results currently are using the random acquisition function.
+pip install -r requirements.txt
+
+## Training via siamese loss
+python experiments.py --dataset "trec6" --acq_fn "random"
+
+## Supervised contrastive learning
+python supcon.py
---
+++ experiments.py
@@ -0,0 +1,271 @@
+from sklearn.metrics import accuracy_score, f1_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sentence_transformers import SentenceTransformer, InputExample, losses, models, datasets, evaluation
+
+from torch.utils.data import DataLoader
+import time
+from sklearn.manifold import TSNE
+import math
+import argparse
+# import warnings filter
+from warnings import simplefilter
+from datasets import load_dataset
+
+from sklearn.manifold import TSNE
+# ignore all future warnings
+simplefilter(action='ignore', category=FutureWarning)
+
+import pandas as pd
+import numpy as np
+#from parallel import DataParallelModel, DataParallelCriterion
+
+import torch
+import random
+import torch
+import pdb
+
+
+
+def set_seed(seed):
+  random.seed(seed)
+  np.random.seed(seed)
+  torch.manual_seed(seed)
+
+
+
+def max_entropy_acquisition_function(logits_b_K_C):
+    return entropy_val(logit_mean(logits_b_K_C, dim=1, keepdim=False), dim=-1)
+
+def entropy_val(logits, dim: int, keepdim: bool = False):
+    return -torch.sum((torch.log(logits) * logits).double(), dim=dim, keepdim=keepdim)
+def logit_mean(logits, dim: int, keepdim: bool = False):
+    r"""Computes $\log \left ( \frac{1}{n} \sum_i p_i \right ) =
+    \log \left ( \frac{1}{n} \sum_i e^{\log p_i} \right )$.
+
+    We pass in logits.
+    """
+    return torch.logsumexp(logits, dim=dim, keepdim=keepdim) - math.log(logits.shape[dim])
+
+def entropy(logits):
+    """
+    Entropy-based uncertainty.
+    see http://robertmunro.com/Uncertainty_Sampling_Cheatsheet_PyTorch.pdf
+    :param prob_dist: output probability distribution
+    :return:
+    """
+
+    if type(logits) is list:
+        logits_B_K_C = torch.stack(logits, 1)  # prob
+    else:
+        logits_B_K_C = logits.unsqueeze(1)  # det
+
+    entropy_scores_ = max_entropy_acquisition_function(logits_B_K_C)
+    return entropy_scores_.cpu().numpy()
+
+
+
+
+
+def sentence_pairs_generation(sentences, labels, pairs):
+	# initialize two empty lists to hold the (sentence, sentence) pairs and
+	# labels to indicate if a pair is positive or negative
+
+  numClassesList = np.unique(labels)
+  idx = [np.where(labels == i)[0] for i in numClassesList]
+
+  for idxA in range(len(sentences)):      
+    currentSentence = sentences[idxA]
+    label = labels[idxA]
+    idxB = np.random.choice(idx[np.where(numClassesList==label)[0][0]])
+    posSentence = sentences[idxB]
+		  # prepare a positive pair and update the sentences and labels
+		  # lists, respectively
+    pairs.append(InputExample(texts=[currentSentence, posSentence], label=1.0))
+
+    negIdx = np.where(labels != label)[0]
+    negSentence = sentences[np.random.choice(negIdx)]
+		  # prepare a negative pair of images and update our lists
+    pairs.append(InputExample(texts=[currentSentence, negSentence], label=0.0))
+  
+	# return a 2-tuple of our image pairs and labels
+  return (pairs)
+
+
+#SST-2
+# Load SST-2 dataset into a pandas dataframe.
+
+def sst_2():
+    train_df = pd.read_csv('data/SST-2/train.tsv', delimiter='\t')
+    train_df, val_df = train_test_split(train_df,test_size=0.1,random_state=42)
+    # Load the test dataset into a pandas dataframe.
+    eval_df = pd.read_csv('data/SST-2/dev.tsv', delimiter='\t')
+    label = "label"
+
+    return train_df,eval_df,label
+
+
+def imdb():
+    train_df = pd.read_csv('data/IMDB/train/train.tsv')
+    train_df, val_df = train_test_split(train_df,test_size=0.1,random_state=42)
+    # Load the test dataset into a pandas dataframe.
+    eval_df = pd.read_csv('data/IMDB/test/test.tsv')
+    label = "sentiment"
+
+    return train_df,eval_df,label
+
+def trec():
+    trec_6 = {}
+    data_all = load_dataset('trec')
+    trec_6['sentence'] = data_all['train']['text']
+    trec_6['label'] = data_all['train']['label-coarse']
+
+    train_df_trec = pd.DataFrame(trec_6)
+    train_df_trec, val_df_trec = train_test_split(train_df_trec,test_size=546,random_state=42)
+    # Load the test dataset into a pandas dataframe.
+    test_df_trec = {}
+    test_df_trec['sentence'] = data_all['test']['text']
+    test_df_trec['label'] = data_all['test']['label-coarse']
+    eval_df_trec = pd.DataFrame(test_df_trec)
+    label = "label" 
+    return train_df_trec,eval_df_trec,label
+
+def dbpedia():
+    train_df = pd.read_csv('data/dbpedia_14/train.csv')
+    # Load the test dataset into a pandas dataframe.
+    eval_df = pd.read_csv('data/dbpedia_14/test.csv')
+    label = "label"
+    train_df_new, train_df_discard, _, _ = train_test_split(train_df, train_df.iloc[:,0], train_size=20000, random_state=42, stratify=train_df.iloc[:,0])
+    return train_df_new, eval_df, label
+
+
+def get_dataset(dataset):
+    if dataset == "sst2":
+        train_df, eval_df, label = sst_2()
+    elif dataset == "trec6":
+        train_df, eval_df, label = trec()
+    elif dataset == "dbpedia":
+        train_df, eval_df, label = dbpedia()
+    else:
+        train_df, eval_df, label = imdb()
+
+    return train_df, eval_df, label
+
+
+
+def aquisition_step(model_lr,X,algo="Entropy"):
+    print("Algo *****"+algo, flush=True)
+    if algo=="Entropy":
+        logits = model_lr.predict_proba(X)
+        logits = torch.Tensor(logits)
+        unc_scores = entropy(logits)
+        new_ids = np.argsort(unc_scores)[::-1][:aquisition_size]
+    else:
+        new_ids = np.random.choice(len(X),aquisition_size) 
+
+    return new_ids
+  
+
+def fit(train_label,train_unlabel, algo="Entropy"):
+    train_label = train_label.reset_index(drop=True)
+    train_unlabel = train_unlabel.reset_index(drop=True)
+
+    x_train = train_label[text_col].values.tolist()
+    y_train = train_label[category_col].values.tolist()
+
+    x_train_unlabel = train_unlabel[text_col].values.tolist()
+    y_train_unlabel = train_unlabel[category_col].values.tolist()
+
+
+
+    train_examples = [] 
+    for x in range(num_itr):
+        train_examples = sentence_pairs_generation(np.array(x_train), np.array(y_train), train_examples)
+    
+    # S-BERT adaptation 
+    
+    train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=32)
+    train_loss = losses.CosineSimilarityLoss(model)
+
+    model.fit(train_objectives=[(train_dataloader, train_loss)], epochs=5, warmup_steps=30, show_progress_bar=True)
+
+    
+    # With Fit (SetFit)
+    X_train = model.encode(x_train)
+    X_eval = model.encode(x_eval)
+    
+    sgd =  LogisticRegression(max_iter=300)
+    sgd.fit(X_train, y_train)
+    y_pred_eval_sgd = sgd.predict(X_eval)
+
+    acc = accuracy_score(y_eval, y_pred_eval_sgd) 
+
+    X_train_unlabel = model.encode(x_train_unlabel)
+    new_ids_label = aquisition_step(sgd,X_train_unlabel,algo)
+    new_ids_unlabel = [i for i in train_unlabel.index.tolist() if i not in new_ids_label]
+    
+
+    train_label_new = train_unlabel.iloc[new_ids_label]
+    train_label = pd.concat([train_label,train_label_new])
+    train_unlabel = train_unlabel.iloc[new_ids_unlabel]
+
+
+    #* num_classes
+    print('Acc. SetFit', accuracy_score(y_eval, y_pred_eval_sgd), flush=True)
+    return train_label,train_unlabel,acc
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", default=None, required=True, type=str, help="Dataset [sst2, trec6, dbpedia, imdb]")
+    parser.add_argument("--acq_fn", default=None, required=True, type=str, help="Acq_fn [Entropy, random]")
+    args = parser.parse_args()
+
+    dataset = args.dataset
+    acq_fn = args.acq_fn
+    
+    train_df, eval_df, label = get_dataset(dataset)
+
+    text_col=train_df.columns.values[0] 
+    category_col=train_df.columns.values[1]
+
+    x_eval = eval_df[text_col].values.tolist()
+    y_eval = eval_df[category_col].values.tolist()
+
+    aquisition_size = len(train_df)//100
+    # st_model = 'sentence-transformers/bert-base-nli-mean-tokens' #@param ['paraphrase-mpnet-base-v2', 'all-mpnet-base-v1', 'all-mpnet-base-v2', 'stsb-mpnet-base-v2', 'all-MiniLM-L12-v2', 'paraphrase-albert-small-v2', 'all-roberta-large-v1']
+    st_model = 'bert-base-uncased' #@param ['paraphrase-mpnet-base-v2', 'all-mpnet-base-v1', 'all-mpnet-base-v2', 'stsb-mpnet-base-v2', 'all-MiniLM-L12-v2', 'paraphrase-albert-small-v2', 'all-roberta-large-v1','sentence-transformers/bert-base-nli-mean-tokens']
+    num_itr = 5 #@param ["1", "2", "3", "4", "5", "10"] {type:"raw"}
+    plot2d_checkbox = False #@param {type: 'boolean'}
+    set_fit_accs = {}
+    no_fit_accs = {}
+
+    # orig_model = SentenceTransformer(st_model)
+    # model = SentenceTransformer(st_model)
+    # train_loss = losses.CosineSimilarityLoss(model)
+
+    
+    acq_iter = 15
+    results =[[] for i in range(acq_iter)]
+    #results_entropy =[[] for i in range(10)]
+    print("Acq_Function ", acq_fn, flush=True)
+    for j in range(5):
+        word_embedding_model = models.Transformer(st_model)
+        pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension(), pooling_mode = 'mean')
+        model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
+
+        set_seed(random.randint(0,100))
+        label_ids = np.random.choice(len(train_df),aquisition_size)
+        unlabel_ids = [i for i in range(len(train_df)) if i not in label_ids]
+        train_label = train_df.iloc[label_ids]
+        train_unlabel = train_df.iloc[unlabel_ids]
+
+        for i in range(acq_iter):
+            train_label,train_unlabel ,acc= fit(train_label,train_unlabel,acq_fn)
+            print(len(train_label), flush=True)
+            results[i].append(acc)
+        print(results, flush=True)
+
+
+
